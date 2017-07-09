fca78c5a27edb53c03da9ee652113637b3f2e63d
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 05:15:11 +0300

    Add Console

---
+++ src/Net.Chdk.Meta.Providers.Software.Sdm.Console/Program.cs
@@ -0,0 +1,81 @@
+﻿using Chimp.Logging.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Net.Chdk.Detectors.Software;
+using Net.Chdk.Detectors.Software.Sdm;
+using Net.Chdk.Encoders.Binary;
+using Net.Chdk.Meta.Providers.Sdm;
+using Net.Chdk.Meta.Providers.Software.Zip;
+using Net.Chdk.Meta.Writers.Software;
+using Net.Chdk.Meta.Writers.Software.Json;
+using Net.Chdk.Model.Software;
+using Net.Chdk.Providers.Boot;
+using Net.Chdk.Providers.Category;
+using Net.Chdk.Providers.Crypto;
+using Net.Chdk.Providers.Product;
+using Net.Chdk.Providers.Software;
+using Net.Chdk.Providers.Software.Sdm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net.Chdk.Meta.Providers.Software.Sdm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: hash2sw input1.zip [input2.zip [...]] hash2sw.json");
+                return;
+            }
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddChimpLogging()
+                .AddCategoryProvider()
+                .AddProductProvider()
+                .AddHashProvider()
+                .AddBootProviderResolver()
+                .AddSingleton<IBinaryDecoder, BinaryDecoder>()
+                .AddCameraProvider()
+                .AddSourceProvider()
+                .AddSoftwareHashProvider()
+                .AddBinarySoftwareDetector()
+                .AddKnownPsBinarySoftwareDetector()
+                .AddSdmSoftwareDetector()
+                .AddSdmAdHocSoftwareDetector()
+                .AddSdmSourceProvider()
+                .AddCategoryMetaProvider()
+                .AddBootMetaProvider()
+                .AddSourceMetaProvider()
+                .AddBuildMetaProvider()
+                .AddCompilerMetaProvider()
+                .AddEncodingMetaProvider()
+                .AddSdmCameraMetaProvider()
+                .AddSdmProductMetaProvider()
+                .AddZipSoftwareMetaProvider()
+                .AddJsonSoftwareWriter()
+                .BuildServiceProvider();
+
+            var hash2sw = GetSoftware(serviceProvider, args);
+
+            WriteSoftware(serviceProvider, args.Last(), hash2sw);
+        }
+
+        private static Dictionary<string, SoftwareInfo> GetSoftware(IServiceProvider serviceProvider, string[] args)
+        {
+            var softwareProvider = serviceProvider.GetService<ISoftwareMetaProvider>();
+            return args.Take(args.Length - 1)
+                .SelectMany(softwareProvider.GetSoftware)
+                .Where(s => s != null)
+                .ToDictionary(s => s.Hash.Values.Values.Single(), s => s);
+        }
+
+        private static void WriteSoftware(IServiceProvider serviceProvider, string path, Dictionary<string, SoftwareInfo> hash2sw)
+        {
+            serviceProvider.GetService<ISoftwareWriter>()
+                .WriteSoftware(path, hash2sw);
+        }
+    }
+}
